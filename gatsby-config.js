933cb7d18662e8b397309f392d4877c813c3197d
Austen Turner <austenpturner@msn.com>
2020-12-12 18:17:48 -0800

    add gatsby site and gh pages

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteMetadata: {
+    title: `Austen Turner`,
+    description: `Web Development Portfolio`,
+    siteUrl: `https://austenpturner.github.io/portfolio/`
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "images",
+    //     path: "./src/images/",
+    //   },
+    //   __key: "images",
+    // },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/inscope-logo.png`, // This path is relative to the root of the site.
+    //   },
+    // },
+  ],
+  pathPrefix: "/portfolio",
+};
