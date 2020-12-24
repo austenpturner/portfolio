f408f61c442421b6a52ae028bccd69a9da5905ba
Austen Turner <austenpturner@msn.com>
2020-12-24 13:30:23 -0800

    add layout component and style settings

---
+++ src/components/layout/layout.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+// import Footer from "../footer/footer";
+// import Header from "../header/header"
+import "../../styles/config.scss";
+import "./layout.module.scss";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      {/* <Header/> */}
+      <div>
+        <main>{children}</main>
+        {/* <Footer /> */}
+      </div>
+    </>
+  )
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
