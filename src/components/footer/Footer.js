0405a1adb7918bb6dfdbacd9a62b99b65840e951
Austen Turner <austenpturner@msn.com>
2020-12-26 17:07:15 -0800

    add social link svgs to footer on mobile screens

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,49 +1,27 @@
 import React from "react";
 import { AnchorLink } from "gatsby-plugin-anchor-links";
 import navItems from "../../content/navigation";
-import GitHubIcon from "../image_files/GitHubIcon";
-import OutlookIcon from "../image_files/OutlookIcon";
-import CodePenIcon from "../image_files/CodePenIcon";
-import LinkedInIcon from "../image_files/LinkedInIcon";
+import SocialLinks from "../socialLinks/SocialLinks";
 import styles from "./footer.module.scss";
 
 const Footer = () => {
-  return (
-    <footer>
-      <ul className={styles.footerLinks}>
-        {navItems.map(({link, title}, index) => {
-          return (
-            <li key={index}>
-                <AnchorLink to={link} title={title}/>
-            </li>
-          )
-        })}
-      </ul>
-      <ul className={styles.socialLinks}>
-        <li>
-          <a href="https://github.com/austenpturner" rel="noreferrer" target="_blank">
-            <GitHubIcon />
-          </a>
-        </li>
-        <li>
-          <a href="mailto:austenpturner@msn.com" rel="noreferrer" target="_blank">
-            <OutlookIcon />
-          </a>
-        </li>
-        <li>
-          <a href="https://codepen.io/austenpturner" rel="noreferrer" target="_blank">
-            <CodePenIcon />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/austenpturner/" rel="noreferrer" target="_blank">
-            <LinkedInIcon />
-          </a>
-        </li>
-      </ul>
-      <p>&copy; 2020 Austen Turner</p>
-    </footer>
-  )
+  	return (
+    	<footer>
+			<ul className={styles.footerLinks}>
+				{navItems.map(({link, title}, index) => {
+					return (
+						<li key={index}>
+							<AnchorLink to={link} title={title}/>
+						</li>
+					)
+				})}
+			</ul>
+			<div>
+				<SocialLinks />
+			</div>
+      		<p>&copy; 2020 Austen Turner</p>
+    	</footer>
+  	)
 };
 
 export default Footer;
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,10 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 import SocialLinks from "../socialLinks/SocialLinks";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
-
 import "../../styles/config.scss";
 import "./layout.module.scss";
 
