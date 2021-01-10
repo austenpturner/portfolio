065007633ed53314500a4bef6610b9dc49163be7
austenpturner <austenpturner@msn.com>
2021-01-10 01:27:05 +0000

    Deploying to gh-pages from  @ 5179f71551808ad565ff4ab97fe6c47f024a79ab 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a9e51caf9637f833666f.js"
+    "url": "webpack-runtime-8053154bb578359600c2.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "75b4dda4411e92740c811d4ff22a70ca"
+    "revision": "fed548bf84e416fbb0f8cf24701558db"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
