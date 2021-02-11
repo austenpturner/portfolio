180bb3320c610fa4f7af6ffc441272ebb3b62632
Austen Turner <austenpturner@msn.com>
2021-02-10 16:23:42 -0800

    edit gatsby-config file system path

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,9 +22,9 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: "./src/images/",
+        path: `${__dirname}/src/images`,
       },
-      __key: "images",
+      // __key: "images",
     },
     // {
     //   resolve: `gatsby-plugin-manifest`,
