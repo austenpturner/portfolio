a27be4de7e24906f4bf3f45a1633b7002645601a
Austen Turner <austenpturner@msn.com>
2025-01-10 09:22:34 -0800

    add useIntersectionObserver hook for scroll effect

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -2,14 +2,21 @@ import me from "../../assets/me.jpg";
 import pothos from "../../assets/pothos.svg";
 import styles from "./about.module.scss";
 import { about } from "../../config/about";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 
 export default function Intro() {
+  const { isVisible, elementRef } = useIntersectionObserver({
+    threshold: 0.2,
+  });
+
   return (
     <div className={styles.about}>
       <img className={styles.profilePic} src={me} alt="me" />
       <div className={styles.content}>
         <h3>About</h3>
-        <p>{about.aboutPara}</p>
+        <p ref={elementRef} className={isVisible ? "visible" : "hidden"}>
+          {about.aboutPara}
+        </p>
       </div>
       <img className={styles.pothosPic} src={pothos} alt="pothos art" />
     </div>
---
+++ src/hooks/useIntersectionObserver.js
@@ -0,0 +1,20 @@
+import { useState, useEffect, useRef } from "react";
+
+export default function useIntersectionObserver(options = {}) {
+  const [isVisible, setIsVisible] = useState(false);
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsVisible(entry.isIntersecting);
+    }, options);
+
+    if (elementRef.current) observer.observe(elementRef.current);
+
+    return () => {
+      if (elementRef.current) observer.unobserve(elementRef.current);
+    };
+  }, [options]);
+
+  return { isVisible, elementRef };
+}
