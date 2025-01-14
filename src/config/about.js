4351d7f31b6b95c6504a9ba15dca481a0e407bbf
Austen Turner <austenpturner@msn.com>
2025-01-14 09:38:35 -0800

    update about content and text alignment

--- src/config/about.js
+++ src/config/about.js
@@ -1,4 +1,4 @@
 export const about = {
   aboutPara:
-    "Hi, I’m Austen. I use responsive design to create intuitive and eye-catching websites. After completing University of Washington’s Full-Stack Development Program, I worked as a Frontent Developer for two small, start-up companies. I've continued to sharpen my skills by building practice projects and completing development challenges through sites like Frontend Mentor. While I have experience with CSS frameworks and WordPress, I like to build my own reusable components with custom styles using React and SCSS.",
+    "Hi, I’m Austen. I use responsive design to create intuitive and eye-catching websites. After completing University of Washington’s Full-Stack Development Program, I worked as a Web Developer for TitanFlow while acquiring leadership skills as Clinical Manager at SeattleNTC. I've continued to sharpen my skills by building practice projects and completing development challenges through sites like Frontend Mentor. While I have experience with CSS frameworks and WordPress, I like to build my own reusable components with custom styles using React and SCSS.",
 };
